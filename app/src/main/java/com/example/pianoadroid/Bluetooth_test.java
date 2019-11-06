da6948b1c1903e93d804c122f818b16a2fdde23c
YOUNGJUYOON1992 <dudwntld@gmail.com>
2019-11-06 16:26:05 +0900

    d

---
+++ app/src/main/java/com/example/pianoadroid/Bluetooth_test.java
@@ -0,0 +1,410 @@
+package com.example.pianoadroid;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+
+public class Bluetooth_test extends AppCompatActivity {
+    private final int REQUEST_BLUETOOTH_ENABLE = 100;
+
+    private TextView mConnectionStatus;
+    private EditText mInputEditText;
+
+    ConnectedTask mConnectedTask = null;
+
+    static BluetoothAdapter mBluetoothAdapter;
+    private String mConnectedDeviceName = null;
+    private ArrayAdapter<String> mConversationArrayAdapter;
+    static boolean isConnectionError = false;
+    private static final String TAG = "BluetoothClient";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_test);
+        Button sendButton = (Button)findViewById(R.id.send_button);
+
+
+
+
+        //아두이노에 데이터 보내기
+        sendButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //Edittext 값 읽어와서 아두이노에 데이터 보내기
+                String sendMessage = mInputEditText.getText().toString();
+                if ( sendMessage.length() > 0 ) {
+                    sendMessage(sendMessage);
+                }
+            }
+        });
+
+        mConnectionStatus = (TextView)findViewById(R.id.connection_status_textview);
+
+        mInputEditText = (EditText)findViewById(R.id.input_string_edittext); //데이터 입력 edit text
+        ListView mMessageListview = (ListView) findViewById(R.id.message_listview);// 데이터 로그 list view
+
+
+        mConversationArrayAdapter = new ArrayAdapter<>( this,
+                android.R.layout.simple_list_item_1 );
+        mMessageListview.setAdapter(mConversationArrayAdapter);
+
+
+        Log.d( TAG, "Initalizing Bluetooth adapter...");
+
+        //BluetoothAdapter 클래스를 얻는 작업부터 시작
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            showErrorDialog("이 기기는 블루투스를 구현하지 않았습니다.");
+            return;
+        }
+
+        // 블루투스 활성/비활성 판단
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, REQUEST_BLUETOOTH_ENABLE);
+        }
+        else {
+            Log.d(TAG, "Initialisation successful.");
+            //블루투스 활성화됨
+            showPairedDevicesListDialog();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if ( mConnectedTask != null ) {
+
+            mConnectedTask.cancel(true);
+        }
+    }
+
+    //아두이노와 안드로이드 연결함
+    private class ConnectTask extends AsyncTask<Void, Void, Boolean> {
+
+        private BluetoothSocket mBluetoothSocket = null;
+        private BluetoothDevice mBluetoothDevice = null;
+
+        ConnectTask(BluetoothDevice bluetoothDevice) {
+            mBluetoothDevice = bluetoothDevice;
+            mConnectedDeviceName = bluetoothDevice.getName();
+
+            //SPP
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+            try {
+                mBluetoothSocket = mBluetoothDevice.createRfcommSocketToServiceRecord(uuid);
+                Log.d( TAG, "create socket for "+mConnectedDeviceName);
+
+            } catch (IOException e) {
+                Log.e( TAG, "socket create failed " + e.getMessage());
+            }
+
+            mConnectionStatus.setText("connecting...");
+        }
+
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            // Always cancel discovery because it will slow down a connection
+            mBluetoothAdapter.cancelDiscovery();
+
+            // Make a connection to the BluetoothSocket
+            try {
+                // This is a blocking call and will only return on a
+                // successful connection or an exception
+                mBluetoothSocket.connect();
+            } catch (IOException e) {
+                // Close the socket
+                try {
+                    mBluetoothSocket.close();
+                } catch (IOException e2) {
+                    Log.e(TAG, "닫을 수 없습니다() " +
+                            " 연결 실패 중 소켓", e2);
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+
+
+        @Override
+        protected void onPostExecute(Boolean isSucess) {
+
+            if ( isSucess ) {
+                // 연결된후 서로 메세지보낼수 있는 메소드
+                connected(mBluetoothSocket);
+            }
+            else{
+
+                isConnectionError = true;
+                Log.d( TAG,  "장치를 연결할 수 없습니다");
+                showErrorDialog("장치를 연결할 수 없습니다");
+            }
+        }
+    }
+
+    //기기와 연결된후 통신하기
+    public void connected( BluetoothSocket socket ) {
+        mConnectedTask = new ConnectedTask(socket);
+        mConnectedTask.execute();
+    }
+
+
+
+    private class ConnectedTask extends AsyncTask<Void, String, Boolean> {
+
+        private InputStream mInputStream = null;
+        private OutputStream mOutputStream = null;
+        private BluetoothSocket mBluetoothSocket = null;
+
+        ConnectedTask(BluetoothSocket socket){
+
+            mBluetoothSocket = socket;
+            try {
+                mInputStream = mBluetoothSocket.getInputStream();
+                mOutputStream = mBluetoothSocket.getOutputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "소켓이 생성되지 않았습니다", e );
+            }
+
+            Log.d( TAG, mConnectedDeviceName+"에 연결");
+            mConnectionStatus.setText( mConnectedDeviceName+"에 연결");
+        }
+
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            byte [] readBuffer = new byte[1024];
+            int readBufferPosition = 0;
+
+
+            while (true) {
+
+                if ( isCancelled() ) return false;
+
+                try {
+
+                    int bytesAvailable = mInputStream.available();
+
+                    if(bytesAvailable > 0) {
+
+                        byte[] packetBytes = new byte[bytesAvailable];
+
+                        mInputStream.read(packetBytes);
+
+                        for(int i=0;i<bytesAvailable;i++) {
+
+                            byte b = packetBytes[i];
+                            if(b == '\n')
+                            {
+                                byte[] encodedBytes = new byte[readBufferPosition];
+                                System.arraycopy(readBuffer, 0, encodedBytes, 0,
+                                        encodedBytes.length);
+                                String recvMessage = new String(encodedBytes, "UTF-8");
+
+                                readBufferPosition = 0;
+
+                                Log.d(TAG, "recv message: " + recvMessage);
+                                publishProgress(recvMessage);
+                            }
+                            else
+                            {
+                                readBuffer[readBufferPosition++] = b;
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+
+                    Log.e(TAG, "disconnected", e);
+                    return false;
+                }
+            }
+
+        }
+
+        @Override
+        protected void onProgressUpdate(String... recvMessage) {
+
+            mConversationArrayAdapter.insert(mConnectedDeviceName + ": " + recvMessage[0], 0);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean isSucess) {
+            super.onPostExecute(isSucess);
+
+            if ( !isSucess ) {
+
+
+                closeSocket();
+                Log.d(TAG, "Device connection was lost");
+                isConnectionError = true;
+                showErrorDialog("Device connection was lost");
+            }
+        }
+
+        @Override
+        protected void onCancelled(Boolean aBoolean) {
+            super.onCancelled(aBoolean);
+
+            closeSocket();
+        }
+
+        void closeSocket(){
+
+            try {
+
+                mBluetoothSocket.close();
+                Log.d(TAG, "close socket()");
+
+            } catch (IOException e2) {
+
+                Log.e(TAG, "unable to close() " +
+                        " socket during connection failure", e2);
+            }
+        }
+
+        void write(String msg){
+
+            msg += "\n";
+
+            try {
+                mOutputStream.write(msg.getBytes());
+                mOutputStream.flush();
+            } catch (IOException e) {
+                Log.e(TAG, "Exception during send", e );
+            }
+
+            mInputEditText.setText(" ");
+        }
+    }
+
+
+    //페어링 기기 검색 .dialog 띄워주기
+    public void showPairedDevicesListDialog()
+    {
+        //페어링 된 기기 목록을 얻으며 페어링 된 기기 정보가 BluetoothDevice 전달됨
+        Set<BluetoothDevice> devices = mBluetoothAdapter.getBondedDevices();
+        final BluetoothDevice[] pairedDevices = devices.toArray(new BluetoothDevice[0]);
+
+        if ( pairedDevices.length == 0 ){
+            showQuitDialog( "페어링 된 기기가 없습니다.\n"
+                    +"다른 기기와 페어링해야합니다.");
+            return;
+        }
+
+        String[] items;
+        items = new String[pairedDevices.length];
+        for (int i=0;i<pairedDevices.length;i++) {
+            items[i] = pairedDevices[i].getName();
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("기기 선택");
+        builder.setCancelable(false);
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                //기기 선택후  기기와 CONNECT함
+                ConnectTask task = new ConnectTask(pairedDevices[which]);
+                task.execute();
+            }
+        });
+        builder.create().show();
+    }
+
+
+
+    public void showErrorDialog(String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Quit");
+        builder.setCancelable(false);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK",  new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                if ( isConnectionError  ) {
+                    isConnectionError = false;
+                    finish();
+                }
+            }
+        });
+        builder.create().show();
+    }
+
+    //블루투스 활성화시 연결되지 않으면 나오는 dialog
+    public void showQuitDialog(String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Quit");
+        builder.setCancelable(false);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK",  new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+        builder.create().show();
+    }
+
+    //아두이노로 데이터 보내기
+    void sendMessage(String msg){
+
+        if ( mConnectedTask != null ) {
+            mConnectedTask.write(msg);
+            Log.d(TAG, "메세지 전송 : " + msg);
+            mConversationArrayAdapter.insert("나 :  " + msg, 0);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == REQUEST_BLUETOOTH_ENABLE){
+            if (resultCode == RESULT_OK){
+                //BlueTooth is now Enabled
+                showPairedDevicesListDialog();
+            }
+            if(resultCode == RESULT_CANCELED){
+                showQuitDialog( "블루투스를 활성화해야합니다");
+            }
+        }
+    }
+
+
+}
+
+
---
+++ app/src/main/java/com/example/pianoadroid/MainActivity.java
@@ -0,0 +1,203 @@
+package com.example.pianoadroid;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity extends AppCompatActivity  implements View.OnTouchListener{
+
+    //좌표를 출력할 텍스트뷰
+ //   private TextView mTvCoord;
+    //터치 위에 원이 그려지는 사용자 정의 뷰
+    private TipsView mTipsView,mTipsView2,mTipsView3,mTipsView4,mTipsView5,mTipsView6,mTipsView7,mTipsView8,mTipsView9;
+
+    //터치 이벤트의 좌표를 받아올 변수
+    private float x = -1, y = -1;
+    //최상단 RelativeLayout
+    private RelativeLayout RL,RL2,RL3,RL4,RL5,RL6,RL7,RL8,RL9;
+    private ImageView imgs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //xml에 정의한 뷰들을 불러옴
+        RL = (RelativeLayout)findViewById(R.id.RL);
+        RL2 = (RelativeLayout)findViewById(R.id.RL2);
+        RL3 = (RelativeLayout)findViewById(R.id.RL3);
+        RL4 = (RelativeLayout)findViewById(R.id.RL4);
+        RL5 = (RelativeLayout)findViewById(R.id.RL5);
+        RL6 = (RelativeLayout)findViewById(R.id.RL6);
+        RL7 = (RelativeLayout)findViewById(R.id.RL7);
+        RL8 = (RelativeLayout)findViewById(R.id.RL8);
+        RL9 = (RelativeLayout)findViewById(R.id.RL9);
+        //mTvCoord = (TextView)findViewById(R.id.tvCoord);
+        mTipsView = (TipsView)findViewById(R.id.tipsView);
+        mTipsView2 = (TipsView)findViewById(R.id.tipsView2);
+        mTipsView3 = (TipsView)findViewById(R.id.tipsView3);
+        mTipsView4 = (TipsView)findViewById(R.id.tipsView4);
+        mTipsView5 = (TipsView)findViewById(R.id.tipsView5);
+        mTipsView6 = (TipsView)findViewById(R.id.tipsView6);
+        mTipsView7 = (TipsView)findViewById(R.id.tipsView7);
+        mTipsView8 = (TipsView)findViewById(R.id.tipsView8);
+        mTipsView9 = (TipsView)findViewById(R.id.tipsView9);
+       // imgs =(ImageView)findViewById(R.id.RefirImage);
+        //텍스트뷰에 텍스트 삽입
+       // mTvCoord.setText("Touch!!!");
+        //Tips뷰를 터치 클릭 리스너 등록
+        mTipsView.setOnTouchListener(this);
+        mTipsView2.setOnTouchListener(this);
+        mTipsView3.setOnTouchListener(this);
+        mTipsView4.setOnTouchListener(this);
+        mTipsView5.setOnTouchListener(this);
+        mTipsView6.setOnTouchListener(this);
+        mTipsView7.setOnTouchListener(this);
+        mTipsView8.setOnTouchListener(this);
+        mTipsView9.setOnTouchListener(this);
+
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dd);
+
+       // imgs.setImageBitmap(bitmap);
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        switch (event.getAction()) {
+            //Down이 발생한 경우
+            case MotionEvent.ACTION_DOWN :
+
+                x = event.getX();
+                y = event.getY();
+
+                //내가 누른 자리의 좌표를 String 값으로 표현할 변수
+                String str;
+                str = "Coordinate : ( " + (int)x + ", " + (int)y + " )";
+              //  mTvCoord.setText(str);
+                //터치 한 곳에 이미지를 표현하기 위해 동적으로 ImageView 생성
+//                ImageView img = new ImageView(this);
+//                img.setImageResource(R.drawable.crotchet_1);
+//
+//
+//                //이미지가 저장될 곳의 x,y좌표를 표현
+//                img.setX(x-40);
+//                img.setY(y - 90);
+//                //최상단 릴레이티브 레이아웃에 이미지를 Add
+//                RL.addView(img);
+                Log.i("SDFSDFSDf", "sdfsdfsdfds"+ v.getTag());
+                if(v.getId() == R.id.tipsView) {
+                    ImageView img = new ImageView(this);
+                    img.setImageResource(R.drawable.crotchet_1);
+
+
+                    //이미지가 저장될 곳의 x,y좌표를 표현
+                    img.setX(x - 40);
+                    img.setY(y - 90);
+                  //  Log.i("SDFSDFSDf", img.getX());
+                    RL.addView(img);
+                }//else if()
+//                        break;
+//        case R.id.RL2:
+//        ImageView img2 = new ImageView(this);
+//        img2.setImageResource(R.drawable.crotchet_1);
+//
+//
+//        //이미지가 저장될 곳의 x,y좌표를 표현
+//        img2.setX(x-40);
+//        img2.setY(y - 90);
+//        RL2.addView(img2);
+//        break;
+//        case R.id.RL3:
+//        ImageView img3 = new ImageView(this);
+//        img3.setImageResource(R.drawable.crotchet_1);
+//
+//
+//        //이미지가 저장될 곳의 x,y좌표를 표현
+//        img3.setX(x-40);
+//        img3.setY(y - 90);
+//        RL3.addView(img3);
+//        break;
+//        case R.id.RL4:
+//        ImageView img4 = new ImageView(this);
+//        img4.setImageResource(R.drawable.crotchet_1);
+//
+//
+//        //이미지가 저장될 곳의 x,y좌표를 표현
+//        img4.setX(x-40);
+//        img4.setY(y - 90);
+//        RL4.addView(img4);
+//        break;
+//        case R.id.RL5:
+//        ImageView img5 = new ImageView(this);
+//        img5.setImageResource(R.drawable.crotchet_1);
+//
+//
+//        //이미지가 저장될 곳의 x,y좌표를 표현
+//        img5.setX(x-40);
+//        img5.setY(y - 90);
+//        RL5.addView(img5);
+//        break;
+//        case R.id.RL6:
+//        ImageView img6 = new ImageView(this);
+//        img6.setImageResource(R.drawable.crotchet_1);
+//
+//
+//        //이미지가 저장될 곳의 x,y좌표를 표현
+//        img6.setX(x-40);
+//        img6.setY(y - 90);
+//        RL6.addView(img6);
+//        break;
+//        case R.id.RL7:
+//        ImageView img7 = new ImageView(this);
+//        img7.setImageResource(R.drawable.crotchet_1);
+//
+//
+//        //이미지가 저장될 곳의 x,y좌표를 표현
+//        img7.setX(x-40);
+//        img7.setY(y - 90);
+//        RL7.addView(img7);
+//        break;
+//        case R.id.RL8:
+//        ImageView img8 = new ImageView(this);
+//        img8.setImageResource(R.drawable.crotchet_1);
+//
+//
+//        //이미지가 저장될 곳의 x,y좌표를 표현
+//        img8.setX(x-40);
+//        img8.setY(y - 90);
+//        RL8.addView(img8);
+//        break;
+//        case R.id.RL9:
+//        ImageView img9 = new ImageView(this);
+//        img9.setImageResource(R.drawable.crotchet_1);
+//
+//
+//        //이미지가 저장될 곳의 x,y좌표를 표현
+//        img9.setX(x-40);
+//        img9.setY(y - 90);
+//        RL9.addView(img9);
+//        break;
+//    }
+                break;
+            //Up이 발생한 경우
+            case MotionEvent.ACTION_UP :
+              //  mTvCoord.setText("Touch !!!");
+                break;
+
+        }
+        //false를 반환하여 뷰 내에 재정의한 onTouchEvent 메소드로 이벤트를 전달한다
+        return false;
+    }
+
+
+}
