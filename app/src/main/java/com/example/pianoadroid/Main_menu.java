8a7b23521be1345d5d085a1b9f7025134245370f
YOUNGJUYOON1992 <dudwntld@gmail.com>
2019-11-06 21:53:14 +0900

    main 기본 화면 완성

--- app/src/main/java/com/example/pianoadroid/Main_menu.java
+++ app/src/main/java/com/example/pianoadroid/Main_menu.java
@@ -2,13 +2,37 @@ package com.example.pianoadroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main_menu extends AppCompatActivity {
 
+    private Button mPlayBtn, mWriteBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        mPlayBtn = (Button)findViewById(R.id.play_btn);  //연주하기
+        mWriteBtn = (Button)findViewById(R.id.write_btn);  // 작곡하기
+
+
+        //연주하기
+        mPlayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // 작곡하기
+        mWriteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
 
 
     }
