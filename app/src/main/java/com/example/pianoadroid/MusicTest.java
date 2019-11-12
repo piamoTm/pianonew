44f7175bed1b0856d2a25c6e0769daea666fd009
juh2 <>
2019-11-12 23:00:41 +0900

    박자감 조절

--- app/src/main/java/com/example/pianoadroid/MusicTest.java
+++ app/src/main/java/com/example/pianoadroid/MusicTest.java
@@ -347,7 +347,7 @@ public class MusicTest extends AppCompatActivity implements MusicTest_Adapter.Th
                             Log.i("testLog", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" + i + "rtest " + musicnote_eng_array[i]);
 
                             // 1초씩 딜레이를 줌
-                            Thread.sleep(800);
+                            Thread.sleep(1200);
                         }
                     }else{
                         for (int i = index_value; i < array.length+1; i++){
@@ -368,14 +368,17 @@ public class MusicTest extends AppCompatActivity implements MusicTest_Adapter.Th
 
                             if (!musicnote_eng_array[i].equals(" ")) {
                                 // 아두이노로 블루투스 통신으로 음계를 보냄
-                                sendMessage(musicnote_eng_array[i]);
+                                String sendMsg = musicnote_eng_array[i] + music.getBeat()[i-1];
+                                Log.i("testLog", "sendMsg "+sendMsg);
+                                //sendMessage(musicnote_eng_array[i]);
+                                sendMessage(sendMsg);
                             }
 
                             //블루투스통신
                             Log.i("testLog", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" + i + "rtest " + musicnote_eng_array[i]);
 
                             // 1초씩 딜레이를 줌
-                            Thread.sleep(800);
+                            Thread.sleep(1200);
                         }
                     }
 
